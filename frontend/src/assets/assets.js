a0c7d2ab8faba9b30440611816d52d4061822655
Sunnyy09 <12001109@csepup.ac.in>
2025-03-26 10:05:31 +0530

    Add: images into assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,287 @@
+import basket_icon from "./basket_icon.png";
+import logo from "./logo.png";
+import header_img from "./header_img.png";
+import search_icon from "./search_icon.png";
+import menu_1 from "./menu_1.png";
+import menu_2 from "./menu_2.png";
+import menu_3 from "./menu_3.png";
+import menu_4 from "./menu_4.png";
+import menu_5 from "./menu_5.png";
+import menu_6 from "./menu_6.png";
+import menu_7 from "./menu_7.png";
+import menu_8 from "./menu_8.png";
+
+import add_icon_white from "./add_icon_white.png";
+import add_icon_green from "./add_icon_green.png";
+import remove_icon_red from "./remove_icon_red.png";
+import app_store from "./app_store.png";
+import play_store from "./play_store.png";
+import linkedin_icon from "./linkedin_icon.png";
+import facebook_icon from "./facebook_icon.png";
+import twitter_icon from "./twitter_icon.png";
+import cross_icon from "./cross_icon.png";
+import selector_icon from "./selector_icon.png";
+import rating_starts from "./rating_starts.png";
+import profile_icon from "./profile_icon.png";
+import bag_icon from "./bag_icon.png";
+import logout_icon from "./logout_icon.png";
+import parcel_icon from "./parcel_icon.png";
+
+export const assets = {
+  logo,
+  basket_icon,
+  header_img,
+  search_icon,
+  rating_starts,
+  add_icon_green,
+  add_icon_white,
+  remove_icon_red,
+  app_store,
+  play_store,
+  linkedin_icon,
+  facebook_icon,
+  twitter_icon,
+  cross_icon,
+  selector_icon,
+  profile_icon,
+  logout_icon,
+  bag_icon,
+  parcel_icon,
+};
+
+export const menuList = [
+  {
+    menu_name: "Salad",
+    menu_image: menu_1,
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_2,
+  },
+  {
+    menu_name: "Deserts",
+    menu_image: menu_3,
+  },
+  {
+    menu_name: "Sandwich",
+    menu_image: menu_4,
+  },
+  {
+    menu_name: "Cake",
+    menu_image: menu_5,
+  },
+  {
+    menu_name: "Pure Veg",
+    menu_image: menu_6,
+  },
+  {
+    menu_name: "Pasta",
+    menu_image: menu_7,
+  },
+  {
+    menu_name: "Noodles",
+    menu_image: menu_8,
+  },
+];
+
+// [
+//   {
+//     _id: "2",
+//     name: "Veg Salad",
+//     image: food_2,
+//     price: 18,
+//     description:
+//       "A refreshing mix of crisp vegetables, tossed in a light dressing for a healthy and delicious meal.",
+//     category: "Salad",
+//   },
+//   {
+//     _id: "3",
+//     name: "Clover Salad",
+//     image: food_3,
+//     price: 16,
+//     description:
+//       "A nutrient-rich salad with fresh greens, nuts, and a zesty dressing to delight your taste buds.",
+//     category: "Salad",
+//   },
+//   {
+//     _id: "4",
+//     name: "Chicken Salad",
+//     image: food_4,
+//     price: 24,
+//     description:
+//       "Tender grilled chicken mixed with fresh vegetables and a tangy dressing for a protein-packed meal.",
+//     category: "Salad",
+//   },
+//   {
+//     _id: "8",
+//     name: "Veg Rolls",
+//     image: food_8,
+//     price: 15,
+//     description:
+//       "A delightful mix of fresh vegetables wrapped in a soft roll with a flavorful sauce.",
+//     category: "Rolls",
+//   },
+//   {
+//     _id: "9",
+//     name: "Ripple Ice Cream",
+//     image: food_9,
+//     price: 14,
+//     description:
+//       "Smooth and creamy ice cream swirled with delicious fruit or chocolate ripples.",
+//     category: "Deserts",
+//   },
+//   {
+//     _id: "12",
+//     name: "Vanilla Ice Cream",
+//     image: food_12,
+//     price: 12,
+//     description:
+//       "Classic vanilla-flavored ice cream with a rich, creamy texture for a timeless treat.",
+//     category: "Deserts",
+//   },
+//   {
+//     _id: "13",
+//     name: "Chicken Sandwich",
+//     image: food_13,
+//     price: 12,
+//     description:
+//       "A delicious sandwich loaded with grilled chicken, lettuce, and mayo in toasted bread.",
+//     category: "Sandwich",
+//   },
+//   {
+//     _id: "14",
+//     name: "Vegan Sandwich",
+//     image: food_14,
+//     price: 18,
+//     description:
+//       "A healthy plant-based sandwich with fresh veggies and a tasty vegan spread.",
+//     category: "Sandwich",
+//   },
+//   {
+//     _id: "15",
+//     name: "Grilled Sandwich",
+//     image: food_15,
+//     price: 16,
+//     description:
+//       "A crispy grilled sandwich filled with a delicious mix of cheese and fresh vegetables.",
+//     category: "Sandwich",
+//   },
+//   {
+//     _id: "16",
+//     name: "Bread Sandwich",
+//     image: food_16,
+//     price: 24,
+//     description:
+//       "A simple yet delicious sandwich made with fresh bread and flavorful fillings.",
+//     category: "Sandwich",
+//   },
+//   {
+//     _id: "17",
+//     name: "Cup Cake",
+//     image: food_17,
+//     price: 14,
+//     description:
+//       "A soft and fluffy cupcake topped with creamy frosting for a delightful treat.",
+//     category: "Cake",
+//   },
+//   {
+//     _id: "18",
+//     name: "Vegan Cake",
+//     image: food_18,
+//     price: 12,
+//     description:
+//       "A rich and moist cake made with plant-based ingredients, perfect for vegans.",
+//     category: "Cake",
+//   },
+//   {
+//     _id: "19",
+//     name: "Butterscotch Cake",
+//     image: food_19,
+//     price: 20,
+//     description:
+//       "A delightful butterscotch-flavored cake topped with caramelized nuts and rich frosting.",
+//     category: "Cake",
+//   },
+//   {
+//     _id: "20",
+//     name: "Sliced Cake",
+//     image: food_20,
+//     price: 15,
+//     description:
+//       "A deliciously moist cake served in perfectly cut slices for easy enjoyment.",
+//     category: "Cake",
+//   },
+//   {
+//     _id: "21",
+//     name: "Garlic Mushroom",
+//     image: food_21,
+//     price: 14,
+//     description:
+//       "Sautéed mushrooms infused with garlic and herbs, a flavorful and healthy dish.",
+//     category: "Pure Veg",
+//   },
+//   {
+//     _id: "22",
+//     name: "Fried Cauliflower",
+//     image: food_22,
+//     price: 22,
+//     description:
+//       "Crispy fried cauliflower bites coated in a flavorful seasoning, perfect for snacking.",
+//     category: "Pure Veg",
+//   },
+//   {
+//     _id: "23",
+//     name: "Mix Veg Pulao",
+//     image: food_23,
+//     price: 10,
+//     description:
+//       "Aromatic basmati rice cooked with a blend of fresh vegetables and Indian spices.",
+//     category: "Pure Veg",
+//   },
+//   {
+//     _id: "24",
+//     name: "Rice Zucchini",
+//     image: food_24,
+//     price: 12,
+//     description:
+//       "A healthy and delicious rice dish with sautéed zucchini and mild seasoning.",
+//     category: "Pure Veg",
+//   },
+//   {
+//     _id: "25",
+//     name: "Cheese Pasta",
+//     image: food_25,
+//     price: 12,
+//     description:
+//       "A rich and creamy pasta dish loaded with melted cheese for ultimate comfort.",
+//     category: "Pasta",
+//   },
+//   {
+//     _id: "26",
+//     name: "Tomato Pasta",
+//     image: food_26,
+//     price: 18,
+//     description:
+//       "Classic pasta tossed in a tangy tomato-based sauce with herbs and spices.",
+//     category: "Pasta",
+//   },
+//
+//   {
+//     _id: "29",
+//     name: "Butter Noodles",
+//     image: food_29,
+//     price: 14,
+//     description:
+//       "Soft and buttery noodles with a touch of herbs for a comforting meal.",
+//     category: "Noodles",
+//   },
+//   {
+//     _id: "30",
+//     name: "Veg Noodles",
+//     image: food_30,
+//     price: 12,
+//     description:
+//       "A flavorful noodle dish tossed with fresh vegetables and Asian spices.",
+//     category: "Noodles",
+//   },
+// ];
